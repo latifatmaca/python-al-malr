da22b8c9cf024ace5404603bafe6d1d288d40b22
Latif Atmaca <57047239+latifatmaca@users.noreply.github.com>
2021-05-07 23:54:16 +0300

    Add files via upload

---
+++ asal_sayı_kontrolü.py
@@ -0,0 +1,65 @@
+
+sayi=int(input("Sayıyı Girin : "))
+if sayi > 1:
+   
+   for i in range(2,sayi):
+       if (sayi % i) == 0:
+           print(sayi," Asal Sayı Değildir.")
+           break
+   else:
+       print(sayi," Asal Sayıdır.")
+ 
+    
+sayi1=int(input("2.Sayıyı Girin : "))
+if sayi1 > 1:
+   
+   for i in range(2,sayi1):
+       if (sayi1 % i) == 0:
+           print(sayi1," Asal Sayı Değildir.")
+           break
+   else:
+       print(sayi1," Asal Sayıdır.")
+       
+       
+sayi2=int(input("3.Sayıyı Girin : "))
+if sayi2 > 1:
+   
+   for i in range(2,sayi2):
+       if (sayi2 % i) == 0:
+           print(sayi2," Asal Sayı Değildir.")
+           break
+   else:
+       print(sayi2," Asal Sayıdır.")
+
+sayi3=int(input("4.Sayıyı Girin : "))
+if sayi3 > 1:
+   
+   for i in range(2,sayi3):
+       if (sayi3 % i) == 0:
+           print(sayi3," Asal Sayı Değildir.")
+           break
+   else:
+       print(sayi3," Asal Sayıdır.")
+
+
+sayi4=int(input("5.Sayıyı Girin : "))
+if sayi4 > 1:
+   
+   for i in range(2,sayi4):
+       if (sayi4 % i) == 0:
+           print(sayi4," Asal Sayı Değildir.")
+           break
+   else:
+       print(sayi4," Asal Sayıdır.")
+
+
+
+
+
+
+
+
+
+
+
+       
---
+++ string_içinden_rakam_çekme_yarım.py
@@ -0,0 +1,24 @@
+#BÖYLE BİRŞEYLER DENEDİM AMA ÇALIŞTIRAMADIM KODU :(
+def TemizVeri():
+    ilk_string="ah5m4t"
+    ikinci_string="m9ehm4et"
+    ucuncu_string="ha3k5a1n"
+    rakamlar={0,1,2,3,4,5,6,7,8,9}
+    for i in ilk_string:
+        for i in rakamlar:
+            if ilk_string[i]==rakamlar[i]:
+                ilk_string.remove(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
